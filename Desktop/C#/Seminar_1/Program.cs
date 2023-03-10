c30f01808b32cad934c043933765f9334c484aac
Mariya <mishutkin869@gmail.com>
2023-03-10 14:05:52 +0300

    Вводим решения задач с семинара 1

---
+++ Desktop/C#/Seminar_1/Program.cs
@@ -0,0 +1,80 @@
+﻿//Задача 1
+//Напишите программу, которая на вход принимает число и выдаёт его квадрат (число умноженное на само себя).
+// Например:
+// 4 -> 16 
+// -3 -> 9 
+// -7 -> 49
+// Вводим переменную int, куда кладем значение от пользоватеся, конвертируем данные.
+// С помощью другой переменной int sqt получаем значение квадрата и выводим на печать.
+
+// Console.WriteLine("Введите текст:");
+// int number = Convert.ToInt32(Console.ReadLine());
+// int sqt = number*number;
+// Console.WriteLine(sqt);
+
+
+// Задача 2
+// Напишите программу, которая на вход принимает два числа и проверяет, 
+// является ли первое число квадратом второго.
+
+// Console.WriteLine("Введите число:");
+// int number1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите число:");
+// int number2 = Convert.ToInt32(Console.ReadLine());
+
+// if(number1==number2*number2)
+// {
+//     Console.WriteLine("Является");
+// }
+// else
+// {
+//     Console.WriteLine("Не является");
+// }
+
+
+
+
+// Задача 3
+// Напишите программу, которая будет выдавать
+//  название дня недели по заданному номеру
+// Console.WriteLine("Введите число");
+// int number1 = Convert.ToInt32(Console.ReadLine());
+
+// if(number1==1)
+// {
+//     Console.WriteLine("Понедельник");
+// }
+// else if(number1==2)
+// {
+//     Console.WriteLine("Вторник");
+
+// }
+// else if(number1==3)
+// {
+//     Console.WriteLine("Среда");
+
+// }
+// else if(number1==4)
+// {
+//     Console.WriteLine("Четверг");
+
+// }
+// else if(number1==5)
+// {
+//     Console.WriteLine("Пятница");
+
+// }
+// else if(number1==6)
+// {
+//     Console.WriteLine("Суббота");
+
+// }
+// else if(number1==7)
+// {
+//     Console.WriteLine("Воскресенье");
+
+// }
+// else
+// {
+//     Console.WriteLine("Такого дня недели у нас нет");
+// }
