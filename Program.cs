60d9e6564a2b2a468982f555d3ccebb8cc5bc9be
Mariya <mishutkin869@gmail.com>
2023-03-14 22:31:06 +0300

    Сделали второе ДЗ для проверки

--- Program.cs
+++ Program.cs
@@ -13,11 +13,27 @@
 
 // Console.WriteLine("Вторая цифра: " +result);
 
+
 // Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, 
 // что третьей цифры нет.
 // 645 -> 5
 // 78 -> третьей цифры нет
 // 32679 -> 6
+
+// Console.WriteLine("Введите число");
+// string anyNumber=Console.ReadLine();
+
+// if (anyNumber.Length>2)
+// {
+//     Console.WriteLine("Третья цифра -> " + anyNumber[2]);
+// }
+// else{
+//     Console.WriteLine("Третьей цифры нет");
+// }
+
+
+
+// Console.WriteLine("Введите число, тобы проверить какой это день недели");
 
 
 // Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
@@ -25,23 +41,18 @@
 // 7 -> да 
 // 1 -> нет
 
-Console.WriteLine("Введите число, тобы проверить какой это день недели");
-int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число, чтобы проверить какой это день недели");
+int dayNum = Convert.ToInt32(Console.ReadLine());
 
-if (num>7&&num<1)
+if (dayNum==6 || dayNum==7)
+{
+    Console.WriteLine("Наконец-то выходной -> ДА!");
+}
+else if (dayNum <1 || dayNum>7)
 {
     Console.WriteLine("Это не день недели");
 }
-else if (num<=5)
-{
-    Console.WriteLine("Будний день, надо поработать");
-}
-if (num=6)
-{
-    Console.WriteLine("Наконец-то выходной!");
-}
-
-
+else Console.WriteLine("Еще надо поработать, этот день не выходной -> НЕТ!");
 
 
 
